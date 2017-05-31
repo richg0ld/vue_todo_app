f3ede04b7887417b7be73b56425d6af2a891a24d
Jung Hyun Kim <corosk@gmail.com>
2017-05-31 17:43:02 +0900

    to do list 껍데기 작업 완료

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import TodoList from './components/TodoList'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    TodoList
   }
 }
 </script>
